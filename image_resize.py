8381b8088dc32945ac86b17978df172c75b467a0
Roman Ch <hello@romangagarin.com>
2018-03-07 08:18:52 +0000

    change image_resize

--- image_resize.py
+++ image_resize.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
     original_size = original_image.size
     original_width, original_height = original_image.size
     output_image = args.output
-    if args.output.endswith('/') and (not os.path.exists(args.output)):
+    if args.output.endswith('/') and not os.path.exists(args.output):
         raise parser.error('Folder doesnt exist')
 
     if args.scale and (args.width or args.height):
